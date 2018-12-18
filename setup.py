40f6a0a6a43a5737c604672c3c279bcd5a1712f3
dizak <dariusz.izak@ibb.waw.pl>
2018-12-18 10:06:10 +0100

    Config files are detected and copied as package data

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from distutils.core import setup
 from setuptools import find_packages
+from glob import glob
 from pubot import __version__, __authors__
 
 
 VERSION = __version__
 AUTHORS = __authors__
 
+config_files = glob('config/*cfg')
+
 setup(
     name='pubot',
     version=VERSION,
     author=AUTHORS,
     packages=find_packages(),
+    include_package_data=True,
+    data_files=[
+        ('config', config_files),
+    ],
     install_requires=open("requirements.txt").readlines(),
     python_requires='>=3.6,',
     long_description=open('README.md').read(),
